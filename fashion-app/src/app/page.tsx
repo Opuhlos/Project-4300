8e26dea36712672c4bb46f4a851298f78fb38bed
opuhlos-eternity <ericnguyen455@gmail.com>
2024-10-21 11:05:29 -0400

    StyleLink top bar wip

--- fashion-app/src/app/page.tsx
+++ fashion-app/src/app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import Navigation from "./components/Navigation";
 
 export default function Home() {
   return (
     <div>
+
+      <div className="mt-4">
+        <Navigation/>
+      </div>
+      
       <p>Hello</p>
+
     </div>
   );
 }
