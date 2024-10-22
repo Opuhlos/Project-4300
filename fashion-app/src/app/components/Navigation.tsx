587e3cebd4690a7439a60209e22986dd8eae692f
opuhlos-eternity <ericnguyen455@gmail.com>
2024-10-22 12:00:23 -0400

    created component for 'StyleLink' title

--- fashion-app/src/app/components/Navigation.tsx
+++ fashion-app/src/app/components/Navigation.tsx
@@ -1,30 +1,19 @@
-import { Space_Grotesk } from '@next/font/google';
+import StyleLink from './StyleLink';
 import NavigationLink from './NavigationLink';
 import NavigationButton from './NavigationButton';
-
-const grotesk = Space_Grotesk({
-    weight: ['400', '700'],
-    subsets: ['latin']
-});
 
 export default function Navigation() {
     return(
         <div className="bg-background mx-10 mt-5 flex justify-between items-center">
 
-            <div className="flex space-x-2">
-                <svg width="30" height="30" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M17.9986 5.53472L35.5997 0.349121L30.464 18.0001L35.5997 35.6012L17.9986 30.4655L0.347656 35.6012L5.53325 18.0001L0.347656 0.349121L17.9986 5.53472Z" fill="black"/>
-                </svg>
+            <StyleLink/>
 
-                <h1 className={`${grotesk.className} font-semibold text-2xl`}>StyleLink</h1>
+            <div className="flex space-x-7 items-center">
+                <NavigationLink label={"About Us"}/>
+                <NavigationLink label={"Creations"}/>
+                <NavigationButton/>
             </div>
-
-        <div className="flex space-x-7 items-center">
-            <NavigationLink label={"About Us"}/>
-            <NavigationLink label={"Creations"}/>
-            <NavigationButton/>
-        </div>
             
-            </div>
+        </div>
     );
 }
