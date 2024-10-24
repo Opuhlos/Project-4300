df07cb444f78379f26016017af5d3bd9a29e0d6c
Opuhlos <140357050+Opuhlos@users.noreply.github.com>
2024-10-23 21:04:03 -0400

    Streamlined font usage

---
+++ fashion-app/src/app/components/Fonts.tsx
@@ -0,0 +1,6 @@
+import { Space_Grotesk } from "next/font/google";
+
+export const grotesk = Space_Grotesk({
+    weight: ['400', '700'],
+    subsets: ['latin']
+});
--- fashion-app/src/app/components/NavigationButton.tsx
+++ fashion-app/src/app/components/NavigationButton.tsx
@@ -1,9 +1,4 @@
-import { Space_Grotesk } from '@next/font/google';
-
-const grotesk = Space_Grotesk({
-    weight: ['400', '700'],
-    subsets: ['latin']
-});
+import { grotesk } from './Fonts';
 
 export default function NavigationButton() {
     return(
