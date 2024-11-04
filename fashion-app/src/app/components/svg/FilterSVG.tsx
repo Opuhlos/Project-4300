fd287b90cc9955de42ca75ab4f0466ef49b4938b
opuhlos-eternity <ericnguyen455@gmail.com>
2024-11-04 09:44:44 -0500

    strokeWidth bug fix

--- fashion-app/src/app/components/svg/FilterSVG.tsx
+++ fashion-app/src/app/components/svg/FilterSVG.tsx
@@ -2,7 +2,7 @@ export default function FilterSVG() {
     return (
         <svg width="35" height="35" viewBox="0 0 47 48" fill="none" xmlns="http://www.w3.org/2000/svg">
         <circle cx="23.5" cy="23.6777" r="23.5" fill="#FFA216"/>
-        <path d="M35.5833 14.8027H11.4166L21.0833 26.2336V34.1361L25.9166 36.5527V26.2336L35.5833 14.8027Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M35.5833 14.8027H11.4166L21.0833 26.2336V34.1361L25.9166 36.5527V26.2336L35.5833 14.8027Z" stroke="white" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
     )
 }
