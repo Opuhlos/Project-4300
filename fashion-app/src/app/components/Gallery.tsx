e7dd24eba06dc9efcfdfb971fd817b586bc3910f
Opuhlos <140357050+Opuhlos@users.noreply.github.com>
2024-11-07 23:06:16 -0500

    tailwindified some css

--- fashion-app/src/app/components/Gallery.tsx
+++ fashion-app/src/app/components/Gallery.tsx
@@ -1,11 +1,10 @@
 import GalleryCard from "./GalleryCard";
-import styles from './Gallery.module.css'
 
 export default function Gallery() {
     // 
     return(
-        // flex flex-wrap gap-x-3 gap-y-3 mx-5 
-        <div className={`gap-y-5 gap-x-2 mx-5 flex-1 ${styles.container}`}>
+        // The 359.3px corresponds to the width of the Gallery Cards
+        <div className={`gap-y-5 gap-x-2 mx-5 flex-1 overflow-y-auto grid grid-cols-[repeat(auto-fit,_359.3px)] justify-center`}>
             <GalleryCard/>
             <GalleryCard/>
             <GalleryCard/>
