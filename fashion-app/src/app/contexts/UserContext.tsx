6be0792c5b30def3f7d0f595103233a72fe6893d
Daniel <daniel.rao@gmail.com>
2024-11-10 15:39:25 -0500

    UserContext Added
    Added usercontext to deal with login/loggout states

---
+++ fashion-app/src/app/contexts/UserContext.tsx
@@ -0,0 +1,33 @@
+import { createContext, useState, ReactNode, useContext } from 'react';
+
+interface UserContextType {
+    isLoggedIn: boolean;
+    login: () => void;
+    logout: () => void;
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+// Provider component
+export const UserProvider = ({ children }: { children: ReactNode }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  
+    const login = () => setIsLoggedIn(true);
+    const logout = () => setIsLoggedIn(false);
+  
+    return (
+      <UserContext.Provider value = {{isLoggedIn, login, logout}}>
+        {children}
+      </UserContext.Provider>
+    );
+  };
+  
+// Hook to use UserContext in other components
+export const useUser = () => {
+    const context = useContext(UserContext);
+    if (!context) {
+      throw new Error('Hook must be used within a UserProvider');
+    }
+    return context;
+};
+    
--- fashion-app/src/app/layout.tsx
+++ fashion-app/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { UserProvider } from './contexts/UserContext';
 
 export const metadata: Metadata = {
   title: "Fashion App",
@@ -12,10 +13,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <UserProvider>
     <html lang="en">
       <body className="bg-background">
         {children}
       </body>
     </html>
+    </UserProvider>
   );
 }
